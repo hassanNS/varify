a5d425a5d64de6e58d880c5cbc1acc793d7557c0
Sheik The Freak <solergiga@yahoo.com>
2014-04-04 11:44:29 -0400

    added base.py

--- tests/cases/base.py
+++ tests/cases/base.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-
+# Class needed for authenticated test cases
 class BaseTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='root',
